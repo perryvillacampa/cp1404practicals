9b01299164878044981e9038b10cd4bfda3be42e
kylev <kylevillacampa0@gmail.com>
2025-09-27 14:13:28 +1000

    Added docstrings to each function

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,15 +1,17 @@
-
 def main():
+    """This programme gets a password and prints asterisks based on the length of password"""
     user_input = get_password()
     if user_input == "Pythonista":
         print_asterisks(user_input)
 
 
 def print_asterisks(user_input):
+    """This function prints asterisks based on the length of password"""
     print(len(user_input) * "*")
 
 
 def get_password():
+    """This function gets a password from the user"""
     user_input = input("Enter a password: ")
     return user_input
 
