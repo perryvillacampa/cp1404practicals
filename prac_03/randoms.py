72e9497720b3bc4aedb824314498ac96aa5c7e8b
kylev <kylevillacampa0@gmail.com>
2025-10-05 22:46:26 +1000

    Added import random to produce a random number form 1 to 100 and added comments

---
+++ prac_03/randoms.py
@@ -0,0 +1,16 @@
+# What did you see on line 1?
+# What was the smallest number you could have seen, what was the largest?
+# The smallest possible number is 5
+
+# What did you see on line 2?
+# What was the smallest number you could have seen, what was the largest?
+# Could line 2 have produced a 4?
+# The smallest number is 3 and the largest number is 9. It could not have produced a 4.
+
+# What did you see on line 3?
+# What was the smallest number you could have seen, what was the largest?
+# The smallest possible number is 2.5 and the largest possible number is 5.5.
+
+import random
+
+print(random.randint(1,100))
