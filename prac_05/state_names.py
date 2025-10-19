9406204b31c9abc608c48d10a701056efdb0b497
kylev <kylevillacampa0@gmail.com>
2025-10-19 22:46:25 +1000

    Complete prac 5 state names exercise

---
+++ prac_05/state_names.py
@@ -0,0 +1,34 @@
+"""
+CP1404/CP5632 Practical
+State names in a dictionary
+File needs reformatting
+"""
+
+CODE_TO_NAME = {
+    "QLD": "Queensland",
+    "NSW": "New South Wales",
+    "NT": "Northern Territory",
+    "WA": "Western Australia",
+    "ACT": "Australian Capital Territory",
+    "VIC": "Victoria",
+    "TAS": "Tasmania",
+    "SA": "South Australia"
+}
+print(CODE_TO_NAME)
+
+print("\n--- All States and Territories ---")
+max_code_length = max(len(code) for code in CODE_TO_NAME)
+
+for code, name in CODE_TO_NAME.items():
+    print(f"{code:<{max_code_length}} is {name}")
+
+print("\n--- State Lookup ---")
+state_code = input("Enter short state: ").upper()
+while state_code != "":
+    try:
+        print(f"{state_code} is {CODE_TO_NAME[state_code]}")
+    except KeyError:
+        print("Invalid short state")
+    state_code = input("Enter short state: ").upper()
+
+print("--- Program finished ---")
