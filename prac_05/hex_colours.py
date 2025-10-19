90074a1e21942ec78db02d872945915c892e9c2c
kylev <kylevillacampa0@gmail.com>
2025-10-19 22:49:33 +1000

    Complete prac 5 hex colours  exercise

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,33 @@
+"""
+CP1404/CP5632 Practical
+Hexadecimal Colour Lookup Program
+"""
+
+COLOUR_CODES = {
+    "ALICEBLUE": "#f0f8ff",
+    "ANTIQUEWHITE": "#faebd7",
+    "AQUAMARINE": "#7fffd4",
+    "BEIGE": "#f5f5dc",
+    "BLUEVIOLET": "#8a2be2",
+    "CHOCOLATE": "#d2691e",
+    "CORAL": "#ff7f50",
+    "DARKGREEN": "#006400",
+    "FIREBRICK": "#b22222",
+    "GHOSTWHITE": "#f8f8ff"
+}
+
+print("--- Hexadecimal Colour Code Lookup ---")
+print("Available colours are:")
+print(", ".join(COLOUR_CODES.keys()))
+
+colour_name = input("Enter a colour name: ").upper()
+
+while colour_name != "":
+    try:
+        code = COLOUR_CODES[colour_name]
+        print(f"The code for {colour_name.title()} is {code}")
+    except KeyError:
+        print("Invalid colour name.")
+    colour_name = input("Enter a colour name: ").upper()
+
+print("Program finished.")
